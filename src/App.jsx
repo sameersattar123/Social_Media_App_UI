e214d914536f19e070d5269ba3318d3a95d30079
Sameer <sameersattar1111@gmail.com>
2023-08-28 04:39:10 +0500

    start  commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Button from '@mui/material/Button';
+import { Typography, styled } from "@mui/material";
 
 const App = () => {
+  const BlueButton = styled(Button)({
+    backgroundColor : "blue",
+    color: "#fff",
+    "&:hover":{
+      backgroundColor : "gray",
+      color : "white"
+    }
+  })
   return (
     <div>
-      <Button variant="contained" color="success" > Text</Button>
-      <Button variant="contained" color="secondary">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button variant="contained" color="success"  size="small">Text</Button>
+      <Button variant="contained" color="secondary" size="median">Contained</Button>
+      <Button variant="outlined" size="large">Outlined</Button>
+      <Typography variant="h1"  component="h1">Sameer Sattar wdwdwdw</Typography>
+      <BlueButton>Sameeer Sattar</BlueButton>
     </div>
   );
 };
